4956e1e6f9e0853fad0ba5f2c43ed296f68fcf93
emiliomezac17 <emilio.meza.c17@gmail.com>
2026-03-05 21:10:43 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -7,7 +7,7 @@
 
 `default_nettype none
 
-module tt_um_emilio_vga_(
+module tt_um_emilio_vga(
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,   // Dedicated outputs
   input  wire [7:0] uio_in,   // IOs: Input path
